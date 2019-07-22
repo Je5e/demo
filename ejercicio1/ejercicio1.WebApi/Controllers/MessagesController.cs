a96d99c5cdd1efaafeebc7eff61eb123d8c6f9e6
jese.alvarado <jese.alvarado@CDS2UNIVO7>
2019-07-22 15:58:33 -0600

    Initial

---
+++ ejercicio1/ejercicio1.WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ejercicio1.WebApi.Controllers
+{
+    public class MessagesController : ApiController
+    {
+
+        // http://[NombreDelServidor]/api/[Controlador]
+
+        //public string Message()
+        //{
+        //    return "¡Hola desde Web Api!";
+        //}
+
+        public string GetMessage()
+        {
+            return "Mapeado a un método Get!";
+        }
+
+        public void PostMessage([FromBody] Message message)
+        {
+            string mensajeEnviadoConLaPeticion = message.Text;
+        }
+    }
+
+   public class Message
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ ejercicio1/ejercicio1.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ejercicio1.WebApi.WebApiApplication" Language="C#" %>
